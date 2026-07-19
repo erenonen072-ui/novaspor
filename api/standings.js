dcab5195770a87c8123c78473a6711f73d0499c2
erenonen072-ui <erenonen072@gmail.com>
2026-07-19 13:16:30 +0300

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,16 +1,28 @@
 export default async function handler(req, res) {
 
-    const response = await fetch(
-        "https://v3.football.api-sports.io/standings?league=203&season=2026",
-        {
-            headers:{
-                "x-apisports-key": process.env.API_KEY
+    try {
+
+        const response = await fetch(
+            "https://v3.football.api-sports.io/standings?league=203&season=2024",
+            {
+                headers:{
+                    "x-apisports-key": process.env.API_KEY
+                }
             }
-        }
-    );
+        );
 
-    const data = await response.json();
 
-    res.status(200).json(data);
+        const data = await response.json();
+
+        res.status(200).json(data);
+
+
+    } catch(error){
+
+        res.status(500).json({
+            error:"Hata oluştu"
+        });
+
+    }
 
 }

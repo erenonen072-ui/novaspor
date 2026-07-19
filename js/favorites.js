76c0b766019e94d33f80ee2d7e2eb39d32d43fca
erenonen072-ui <erenonen072@gmail.com>
2026-07-19 13:41:11 +0300

    Create favorites.js

---
+++ js/favorites.js
@@ -0,0 +1,35 @@
+function saveFavorite(){
+
+    const team =
+    document.getElementById("teamSelect").value;
+
+
+    localStorage.setItem(
+        "favoriteTeam",
+        team
+    );
+
+
+    showFavorite();
+
+}
+
+
+
+function showFavorite(){
+
+    const team =
+    localStorage.getItem("favoriteTeam");
+
+
+    if(team){
+
+        document.getElementById("favoriteText").innerHTML =
+        "⭐ Favori takımın: " + team;
+
+    }
+
+}
+
+
+showFavorite();

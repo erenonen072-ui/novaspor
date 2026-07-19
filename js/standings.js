f180dfd443f13fba5924d8e03b3c9f8bd3c0c98a
erenonen072-ui <erenonen072@gmail.com>
2026-07-19 12:46:05 +0300

    Create standings.js

---
+++ js/standings.js
@@ -0,0 +1,55 @@
+async function loadStandings(){
+
+    const box = document.getElementById("standings");
+
+    if(!box) return;
+
+    try{
+
+        const response = await fetch("/api/standings");
+
+        const data = await response.json();
+
+
+        const table = data.response[0].league.standings[0];
+
+
+        box.innerHTML = "";
+
+
+        table.forEach(team => {
+
+
+            box.innerHTML += `
+
+            <div class="standing">
+
+                <span>
+                ${team.rank}. ${team.team.name}
+                </span>
+
+                <strong>
+                ${team.points} P
+                </strong>
+
+            </div>
+
+            `;
+
+
+        });
+
+
+    }catch(error){
+
+        box.innerHTML =
+        "Puan durumu yüklenemedi.";
+
+        console.error(error);
+
+    }
+
+}
+
+
+loadStandings();

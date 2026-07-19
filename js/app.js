5975b3b8aeba26bfcf443b8389ca433e5802e464
erenonen072-ui <erenonen072@gmail.com>
2026-07-20 00:27:13 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log("NovaSpor başladı");
+
+const matches = document.getElementById("matches");
+
+if(matches){
+    matches.innerHTML = `
+        <div>
+            ⚽ Canlı maç sistemi hazırlanıyor...
+        </div>
+    `;
+}

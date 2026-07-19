412357619abfbf9902b4bc5ff2ed591745b0dc2d
erenonen072-ui <erenonen072@gmail.com>
2026-07-20 00:29:06 +0300

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,15 +1,25 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      `https://api.sportmonks.com/v3/football/livescores/inplay?api_token=${process.env.SPORTMONKS_API_TOKEN}&include=participants;scores`
-    );
 
-    const data = await response.json();
+    try {
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
-  }
+        const response = await fetch(
+            "https://api.sportmonks.com/v3/football/fixtures?api_token=" 
+            + process.env.SPORTMONKS_API_TOKEN
+        );
+
+
+        const data = await response.json();
+
+
+        res.status(200).json(data);
+
+
+    } catch(error) {
+
+        res.status(500).json({
+            error:error.message
+        });
+
+    }
+
 }

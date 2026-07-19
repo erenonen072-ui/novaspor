afde40e2a3a9cd31a274765b2d2df84c23bdebb8
erenonen072-ui <erenonen072@gmail.com>
2026-07-19 13:12:33 +0300

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,3 +1,29 @@
-export default function handler(req, res) {
-    res.status(200).send("Standings çalışıyor");
+export default async function handler(req, res) {
+
+    try {
+
+        const response = await fetch(
+            "https://v3.football.api-sports.io/standings?league=203&season=2025",
+            {
+                headers:{
+                    "x-apisports-key": process.env.API_KEY
+                }
+            }
+        );
+
+
+        const data = await response.json();
+
+
+        res.status(200).json(data);
+
+
+    } catch(error){
+
+        res.status(500).json({
+            error:"Hata oluştu"
+        });
+
+    }
+
 }

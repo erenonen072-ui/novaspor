d34c26d1bae5ee6e5374c201502d78be2a4544f9
erenonen072-ui <erenonen072@gmail.com>
2026-07-21 00:02:50 +0300

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -2,24 +2,23 @@ export default async function handler(req, res) {
 
     try {
 
-        const today = new Date().toISOString().split("T")[0];
-
-
         const response = await fetch(
-            `https://www.thesportsdb.com/api/v1/json/123/eventsday.php?d=${today}&s=Soccer`
+            "https://v3.football.api-sports.io/fixtures?live=all",
+            {
+                headers: {
+                    "x-apisports-key": process.env.API_KEY
+                }
+            }
         );
-
 
         const data = await response.json();
 
-
         res.status(200).json(data);
 
-
-    } catch(error){
+    } catch (error) {
 
         res.status(500).json({
-            error:error.message
+            error: error.message
         });
 
     }

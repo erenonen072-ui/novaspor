6ef8805d1e70c3d53bd7a77b7ae38bdf8555d1de
erenonen072-ui <erenonen072@gmail.com>
2026-07-21 01:25:27 +0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,9 +1,23 @@
-export default async function handler(req, res) { 
+export default async function handler(req, res) {
 
-    try { 
+    try {
+
+        const now = new Date();
+
+        let season;
+
+
+        // Futbol sezonu genelde ağustos ayında başlar
+        if (now.getMonth() + 1 >= 8) {
+            season = now.getFullYear();
+        } else {
+            season = now.getFullYear() - 1;
+        }
+
+
 
         const response = await fetch(
-            "https://v3.football.api-sports.io/fixtures?league=203&season=2024",
+            `https://v3.football.api-sports.io/fixtures?league=203&season=${season}`,
             {
                 headers: {
                     "x-apisports-key": process.env.API_KEY
@@ -18,11 +32,14 @@ export default async function handler(req, res) {
         res.status(200).json(data);
 
 
+
     } catch (error) {
+
 
         res.status(500).json({
             error: error.message
         });
+
 
     }
 

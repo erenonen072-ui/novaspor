578c32f8d5692e67675b8f06841e07fd8a53215d
erenonen072-ui <erenonen072@gmail.com>
2026-07-21 00:16:38 +0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,1 +1,23 @@
-https://v3.football.api-sports.io/fixtures?league=203&season=2026
+export default async function handler(req, res) {
+    try {
+        const response = await fetch(
+            "https://v3.football.api-sports.io/fixtures?league=203&season=2026",
+            {
+                headers: {
+                    "x-apisports-key": process.env.API_KEY
+                }
+            }
+        );
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.message
+        });
+
+    }
+}

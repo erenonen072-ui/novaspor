97696686d833b04a636c383622bf036b265d5bc4
erenonen072-ui <erenonen072@gmail.com>
2026-07-23 04:26:33 +0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -2,84 +2,27 @@ export default async function handler(req, res) {
 
     try {
 
-
-        const now = new Date();
-
-        let season;
-
-
-        // Otomatik sezon
-        if (now.getMonth() + 1 >= 8) {
-            season = now.getFullYear();
-        } else {
-            season = now.getFullYear() - 1;
-        }
-
-
-
-        async function getFixtures(year) {
-
-
-            const response = await fetch(
-
-                `https://v3.football.api-sports.io/fixtures?league=203&season=${year}`,
-
-                {
-                    headers: {
-                        "x-apisports-key": process.env.API_KEY
-                    }
+        const response = await fetch(
+            "https://v3.football.api-sports.io/fixtures?league=203&season=2025",
+            {
+                headers: {
+                    "x-apisports-key": process.env.API_KEY
                 }
-
-            );
-
-
-            return await response.json();
+            }
+        );
 
 
-        }
-
-
-
-        let data = await getFixtures(season);
-
-
-
-        // Veri yoksa önceki sezon
-        if (!data.response || data.response.length === 0) {
-
-            data = await getFixtures(season - 1);
-
-        }
-
-
-
-        // Gelecek maçları bırak
-        if (data.response) {
-
-            data.response = data.response.filter(match => {
-
-                return new Date(match.fixture.date) >= new Date();
-
-            });
-
-
-        }
-
+        const data = await response.json();
 
 
         res.status(200).json(data);
 
 
-
     } catch(error) {
 
-
         res.status(500).json({
-
-            error: error.message
-
+            error:error.message
         });
-
 
     }
 

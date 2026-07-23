f9d2ed430eba43f4bd1c639985d3063ed2e30275
erenonen072-ui <erenonen072@gmail.com>
2026-07-23 18:52:51 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -435,3 +435,32 @@ item.style.display="none";
 
 
 }
+// 📱 MOBİL MENÜ
+
+
+const menuBtn =
+document.getElementById("menuBtn");
+
+
+const mobileMenu =
+document.getElementById("mobileMenu");
+
+
+
+if(menuBtn && mobileMenu){
+
+
+menuBtn.addEventListener(
+"click",
+()=>{
+
+
+mobileMenu.classList.toggle(
+"active"
+);
+
+
+});
+
+
+}
